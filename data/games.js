f310933c27fbcb6d900b384b8bb0c337121504f5
jyotishman-web <jyotishmanpathak25@gmail.com>
2026-02-26 07:47:29 +0530

    Add GAMES array with game details

---
+++ data/games.js
@@ -0,0 +1,12 @@
+const GAMES = [
+  {
+    name: "Car Game",
+    page: "games/car.html",
+    thumb: "assets/images/car.png"
+  },
+  {
+    name: "Snake Game",
+    page: "games/snake.html",
+    thumb: "assets/images/snake.png"
+  }
+];
