a602c3004936fd3e561adf7ca14479a32f44ebf9
jyotishman-web <jyotishmanpathak25@gmail.com>
2026-02-27 22:23:49 +0530

    Refactor games list to include categories

--- data/games.js
+++ data/games.js
@@ -1,12 +1,24 @@
 const GAMES = [
-  {
-    name: "Car Game",
-    page: "games/car.html",
-    thumb: "assets/images/car.jpg"
-  },
-  {
-    name: "Snake Game",
-    page: "games/snake.html",
-    thumb: "assets/images/snake.png"
-  }
+
+{
+  name: "Car Game",
+  thumb: "assets/images/car.jpg",
+  page: "games/car.html",
+  category: "racing"
+},
+
+{
+  name: "Puzzle Game",
+  thumb: "assets/images/puzzle.jpg",
+  page: "games/puzzle.html",
+  category: "puzzle"
+},
+
+{
+  name: "Shooting Game",
+  thumb: "assets/images/shoot.jpg",
+  page: "games/shoot.html",
+  category: "action"
+}
+
 ];
