95870c277e30a21ec05e3e6ae6bf2e368f318567
jyotishman-web <jyotishmanpathak25@gmail.com>
2026-02-28 05:58:59 +0530

    Update game rendering to use category filtering
    Refactor rendering logic to filter games by category.

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-const featured = document.getElementById("featured");
+function render(sectionId, category) {
+  const container = document.getElementById(sectionId);
 
-GAMES.forEach(game => {
-  featured.innerHTML += `
-    <div class="card">
-      <a href="${game.page}">
-        <img src="${game.thumb}">
-        <h3>${game.name}</h3>
-      </a>
-    </div>
-  `;
-});
+  GAMES
+    .filter(g => g.category === category)
+    .forEach(game => {
+      container.innerHTML += `
+        <div class="card">
+          <a href="${game.page}">
+            <img src="${game.thumb}">
+            <h3>${game.name}</h3>
+          </a>
+        </div>
+      `;
+    });
+}
+
+render("featured", "featured");
+render("popular", "popular");
+render("new", "new");
